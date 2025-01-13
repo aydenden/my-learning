c2a4d7732b522e2e1206f8766845f10f16bfb097
yoonhona <lucky72nyh@gmail.com>
2025-01-14 08:15:32 +0900

    feat: adopted microfrontends 로컬 기본 설정

--- adopted-microfrontends-nextjs/apps/web/next.config.js
+++ adopted-microfrontends-nextjs/apps/web/next.config.js
@@ -1,4 +1,27 @@
+const isLocal = process.env.NODE_ENV === "development";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/docs",
+        destination: "http://localhost:3001/docs",
+      },
+      {
+        source: "/docs/:path*",
+        destination: "http://localhost:3001/docs/:path*",
+      },
+      {
+        source: "/books",
+        destination: "http://localhost:3002/books",
+      },
+      {
+        source: "/books/:path*",
+        destination: "http://localhost:3002/books/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
