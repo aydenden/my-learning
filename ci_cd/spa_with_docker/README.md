a2522a48d0fe646949ff6f4ed2c78541623d0785
aydenden <lucky72nyh@gmail.com>
2025-03-21 08:30:02 +0900

    SPA CI/CD

---
+++ ci_cd/spa_with_docker/README.md
@@ -0,0 +1,15 @@
+# On-Premise SPA docker 배포하기
+
+1. SPA Dockerfile 작성
+2. Docker 빌드 최적화
+3. 실제 배포 프로세스 구성
+   1. Blue-Green 배포 구성학기 위한 docker-compose 파일 작성
+   2. Blue-Green 배포를 위한 Nginx 설정
+4. CI/CD 파이프라인 구성
+   1. GitHub Actions를 이용한 CI/CD 파이프라인 구성
+   2. 로컬에 GitHub Actions Runner 구성해서 테스트 해보기
+5. 블로그 포스팅
+
+# On-Premise Next.js docker 배포하기
+
+to-be-continued
--- nextjs/getting-started/nextjs-dashboard/app/ui/fonts.ts
+++ nextjs/getting-started/nextjs-dashboard/app/ui/fonts.ts
@@ -13,7 +13,7 @@ export const lusitana = Lusitana({
  * 그렇기에 로컬 폰트를 최적화하려면 자체적인 최적화가 필요함
  * 물론 이렇게 사용하지 않고 pnpm add pretendard 한후에 pretendard-dynamic-subset.css import하여 사용하면 커뮤니티에서 생성해놓은 서브셋을 사용 가능, 출근해서 PR해야겠네요...!
  *
- * 역시나 토스는 하고 있음, https://tossinvest.com/ 접속하여 네트워크 -> font를 확인해보면 자체 폰트를 서브셋하여 최적화된 폰트들이 다운됨
+ * 역시나 토스는 하고 있음, https://tossinvest.com/ 접속하여 네트워크 -> font를 확인해보면 자체 폰트를 서브셋 최적화된 폰트들이 다운됨
  */
 export const pretendard = localFont({
     src: '../fonts/Pretendard-Regular.woff2',
