9a25cd2c5db6074a47b7119bc071d7a8281adb95
aydenden <lucky72nyh@gmail.com>
2025-03-22 16:22:56 +0900

    feat: react 예제 코드 옮기기

--- react19/src/step/thinking-in-react/index.tsx
+++ react19/src/step/thinking-in-react/index.tsx
@@ -1,5 +1,158 @@
+import { JSX, useState } from "react";
+
+interface Product {
+  category: string;
+  price: string;
+  stocked: boolean;
+  name: string;
+}
+
+interface FilterableProductTableProps {
+  products: Product[];
+}
+
+interface ProductCategoryRowProps {
+  category: string;
+}
+
+interface ProductRowProps {
+  product: Product;
+}
+
+interface ProductTableProps {
+  products: Product[];
+  filterText: string;
+  inStockOnly: boolean;
+}
+
+interface SearchBarProps {
+  filterText: string;
+  inStockOnly: boolean;
+  onFilterTextChange: (text: string) => void;
+  onInStockOnlyChange: (inStock: boolean) => void;
+}
+
+function FilterableProductTable({ products }: FilterableProductTableProps) {
+  const [filterText, setFilterText] = useState("");
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+    </div>
+  );
+}
+
+function ProductCategoryRow({ category }: ProductCategoryRowProps) {
+  return (
+    <tr>
+      <th colSpan={2}>{category}</th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }: ProductRowProps) {
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({
+  products,
+  filterText,
+  inStockOnly,
+}: ProductTableProps) {
+  const rows: JSX.Element[] = [];
+  let lastCategory: string | null = null;
+
+  products.forEach((product) => {
+    if (product.name.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    if (inStockOnly && !product.stocked) {
+      return;
+    }
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />,
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function SearchBar({
+  filterText,
+  inStockOnly,
+  onFilterTextChange,
+  onInStockOnlyChange,
+}: SearchBarProps) {
+  return (
+    <form>
+      <input
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)}
+      />
+      <label>
+        <input
+          type="checkbox"
+          checked={inStockOnly}
+          onChange={(e) => onInStockOnlyChange(e.target.checked)}
+        />{" "}
+        Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+const PRODUCTS: Product[] = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
 const ThinkingInReact = () => {
-  return <>ThinkingInReact</>;
+  return <FilterableProductTable products={PRODUCTS} />;
 };
 
 export default ThinkingInReact;
