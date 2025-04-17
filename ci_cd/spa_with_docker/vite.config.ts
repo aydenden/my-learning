7ad1d5c8dfe9edde651cbfdd034fd607cdad97db
aydenden <lucky72nyh@gmail.com>
2025-04-18 07:41:08 +0900

    fix: Docker 에서 빌드하는 형태로 변경

--- ci_cd/spa_with_docker/vite.config.ts
+++ ci_cd/spa_with_docker/vite.config.ts
@@ -4,4 +4,22 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          const ext = info[info.length - 1];
+
+          // 폰트 파일들을 assets/font 폴더로 이동
+          if (/ttf|otf|eot|woff|woff2/i.test(ext)) {
+            return "assets/font/[name]-[hash][extname]";
+          }
+
+          // 기타 에셋들
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
