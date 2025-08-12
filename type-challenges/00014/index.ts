aed545981b4d704065c7a39092e9dd9b251a38a7
aydenden <lucky72nyh@gmail.com>
2025-08-12 20:03:46 +0900

    docs: llm 설명 추가

--- type-challenges/00014/index.ts
+++ type-challenges/00014/index.ts
@@ -8,6 +8,14 @@
 type First<T extends Array<any>> = T[0];
 // github 모법 답안
 // 빈 배열일 경우 never를 type 반환
+/**
+ * llm 설명
+ * TypeScript에서 빈 배열의 첫 번째 요소 타입을 never로 반환하는 것은 타입 안전성을 높이는 데 도움이 됩니다.
+ * 이유:
+ * never는 "절대 발생하지 않는 값"을 의미합니다. 즉, 빈 배열에서 첫 번째 요소를 얻으려는 시도는 논리적으로 불가능하므로 never가 적합합니다.
+ * 만약 T[0]만 사용하면, 빈 배열의 경우 undefined가 반환됩니다. 하지만 타입 시스템에서는 실제로 값이 없다는 것을 명확히 표현하기 위해 never가 더 엄격합니다.
+ * never를 반환하면, 이후 해당 타입을 사용하는 코드에서 실수로 값을 할당하거나 접근하려고 할 때 타입 에러가 발생하여 버그를 사전에 방지할 수 있습니다.
+ */
 type First2<T extends Array<any>> = T extends [] ? never : T[0];
 
 type arr1 = ["a", "b", "c"];
