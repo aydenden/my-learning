2fa4d86caac6ffeacfc6b2b818d5d8756a0978d0
aydenden <lucky72nyh@gmail.com>
2025-09-07 20:40:54 +0900

    feat: 알고리즘 스터디 1주차

---
+++ leetcode/561.array-partition.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=561 lang=typescript
+ *
+ * [561] Array Partition
+ */
+
+// @lc code=start
+// O(n log n)
+function arrayPairSum(nums: number[]): number {
+    const sorted = nums.sort((a, b) => a-b);
+
+    let result = 0;
+    for(let i = 0; i < nums.length; i+=2) {
+        result += Math.min(sorted[i], sorted[i +1]);
+    }
+
+    return result
+};
+// @lc code=end
+
+
+// @lc code=start
+function arrayPairSum2(nums: number[]): number {
+    const sorted = nums.sort((a, b) => a-b);
+
+    let result = 0;
+    for(let i = 0; i < nums.length; i+=2) {
+        // 정렬을 하였기 때문에 첫번째가 무조건 작음
+        result += sorted[i];
+    }
+
+    return result
+};
+// @lc code=end
+
